ef17465d6897079890ea1454f7a6b14357ae8029
gaolijun <15801419993@163.com>
2017-12-25 17:09:00 +0800

    first

---
+++ js/port.js
@@ -0,0 +1,176 @@
+2017.11.11司机抽奖活动,活动已结束
+原型图
+https://prototype.yongche.org/hujingjing/%E5%8F%8C%E5%8D%81%E4%BA%8C%E6%B4%BB%E5%8A%A8/index.html#g=1&p=双十二抽奖活动
+
+16825825825
+16886318947  0001
+16820171161
+16886318957 测试手机号
+
+
+
+     protected $award_arr = [
+        1 => '门票2张',
+        2 => '空气净化器',
+        3 => '5折打车券',
+        4 => '7折打车券',
+        5 => '8折打车券',
+        6 => '9折打车券'
+    ];
+
+  ��ʼҳ��
+
+http://yaoyakun.market.yongche.org/Miscellaneous/Kooza/getInitPage
+
+���ز���
+
+{
+    "code": 200,
+    "msg": "OK",
+    "result": {
+        "count": 0,
+        "isLogin": 1,
+        "user_id": "8060",
+        "my_awards": [
+            {
+                "award_type": 6,
+                "remark": "非高峰9折优惠券（最高减20元）"
+            },
+            {
+                "award_type": 6,
+                "remark": "非高峰9折优惠券（最高减20元）"
+            },
+            {
+                "award_type": 6,
+                "remark": "非高峰9折优惠券（最高减20元）"
+            },
+            {
+                "award_type": 6,
+                "remark": "非高峰9折优惠券（最高减20元）"
+            },
+            {
+                "award_type": 6,
+                "remark": "非高峰9折优惠券（最高减20元）"
+            },
+            {
+                "award_type": 6,
+                "remark": "非高峰9折优惠券（最高减20元）"
+            },
+            {
+                "award_type": 6,
+                "remark": "非高峰9折优惠券（最高减20元）"
+            },
+            {
+                "award_type": 3,
+                "remark": "5折优惠券（最高减30元）"
+            },
+            {
+                "award_type": 5,
+                "remark": "8折优惠券（最高减10元）"
+            },
+            {
+                "award_type": 6,
+                "remark": "非高峰9折优惠券（最高减20元）"
+            }
+        ],
+        "userList": [
+            {
+                "name": "测*",
+                "cellphone": "168****9895",
+                "award_type": 6
+            },
+            {
+                "name": "测*",
+                "cellphone": "168****9895",
+                "award_type": 6
+            },
+            {
+                "name": "测*",
+                "cellphone": "168****9895",
+                "award_type": 6
+            },
+            {
+                "name": "测*",
+                "cellphone": "168****9895",
+                "award_type": 6
+            },
+            {
+                "name": "测*",
+                "cellphone": "168****9895",
+                "award_type": 6
+            },
+            {
+                "name": "测*",
+                "cellphone": "168****9895",
+                "award_type": 6
+            },
+            {
+                "name": "测*",
+                "cellphone": "168****9895",
+                "award_type": 6
+            },
+            {
+                "name": "测*",
+                "cellphone": "168****9895",
+                "award_type": 6
+            },
+            {
+                "name": "测*",
+                "cellphone": "168****9895",
+                "award_type": 5
+            },
+            {
+                "name": "测*",
+                "cellphone": "168****9895",
+                "award_type": 3
+            }
+        ]
+    }
+}
+
+
+�齱
+
+http://yaoyakun.market.yongche.org/Miscellaneous/Kooza/getResult
+
+����ֵ
+
+{
+    "code": 200,
+    "msg": "�Ǹ߷�9���Ż�ȯ����߼�20Ԫ��",
+    "result": {
+        "award_type": 6 // �н�����
+    }
+}
+
+
+ * code˵��
+ *
+ * 200 OK
+ * 101 �δ��ʼ
+ * 102 ��ѽ���
+ * 201 �û�������
+ * 203 ���¼���ٳ齱
+ * 204 ���û�����ϳ齱����
+ * 205 ��ҵ�û����ܲμӳ齱
+  
+
+
+活动规则：
+["1、活动时间：2017年12月15日18:00起至2017年12月18日23:59止；",
+"2、获取条件：在活动时间内每次完成订单均可参与抽奖，每人可获得的奖品为价值380元或580元KOOZA太阳马戏门票2张、车载空气净化器1台、5折易到打车券、7折易到打车券、8折易到打车券、9折易到打车券；",
+"3、领取方式：获得KOOZA太阳马戏门票和车载空气净化器的用户在中奖后请填写弹出页面中的收件人信息，当日完成的订单请在当日23:59前填写完成，逾期视为自动放弃，我们会在第一时间安排顺丰进行邮寄（暂不支持自取）；获得易到打车券的用户我们会在3个工作日内将优惠券存入账户中；",
+"4、为您提供的KOOZA太阳马戏门票价值380元或580元（具体演出时间与票价及座位随机、门票不含朝阳公园门票、儿童无论大小均需持票入场），地点为北京市朝阳公园北一门外，演出时长在150分钟左右（含30分钟中场休息），观看演出时请遵守场馆的相关规定并服从工作人员安排；",
+"5、易到打车券有效期为7天，5折打车券最高减30元、7折优惠券最高减15元、8折优惠券最高减10元、非高峰9折优惠券最高减10元；",
+"6、本活动仅针对北京地区的个人用户，企业用户不在活动范围之内，更多优惠活动敬请关注易到APP；",
+"7、本活动最终解释权在法律允许范围内归易到所有，活动仅提供给正当、合法使用叫车服务的的易到客户，一旦发现作弊行为，易到有权追回作弊所得门票并保留取消作弊人后续参与易到任何活动的权利，必要时会追究其法律责任。"
+]
+
+手机
+http://i1.yongche.name/media/g2/M01/1A/2B/rBEBJVozJImIQxq_AAA6N3G0LAcAAKYNwNjIWoAADpP284.png
+上滑
+http://i2.yongche.name/media/g2/M01/1A/2B/rBEBJVozJImIKYLBAAA8xMm14G8AAKYNwNjdcsAADzc048.png
+关闭
+http://i1.yongche.name/media/g2/M03/1A/21/rBEBP1ozJIiIMCIUAAADXsqEGDEAAKUjgP__IoAAAN2832.png
+
+https://i1.sandbox.yongche.org/media/g2/M03/1A/21/rBEBP1ozJIiIMCIUAAADXsqEGDEAAKUjgP__IoAAAN2832.png
